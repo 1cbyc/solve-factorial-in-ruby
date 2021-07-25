b092a8087568bc144228e38d0cc6352c3bac49f4
Isaac Nsisong <35623334+isaacnsisong@users.noreply.github.com>
2021-07-25 14:14:48 +0100

    Create factorial.rb

---
+++ factorial.rb
@@ -0,0 +1,10 @@
+# Created by Nsisong
+
+def factorial(n)
+    (1..n).inject(:*) || 1
+end
+
+puts factorial(4)
+
+#Credits to Isaac Nsisong 
+# note that at the print function defined as "puts facrorial" you better input the..em...number you want to turn to factorial.
